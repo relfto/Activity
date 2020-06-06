90cd8477be9354814bea9d1d73f63aa75940e17a
relfto <66303314+relfto@users.noreply.github.com>
2020-06-06 10:11:47 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/task4/MainActivity.kt
+++ app/src/main/java/com/example/task4/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.task4
-
+import android.widget.Button
+import android.widget.TextView
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,10 +11,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        var count = 0;
+        val rb = findViewById<Button>(R.id.Bt2)
+        val rr = findViewById<TextView>(R.id.tx)
+        rb.setOnClickListener {
+            count++;
+            rr.text = count.toString();
 
+        
         Bt1.setOnClickListener{
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
+            }
         }
     }
 }
