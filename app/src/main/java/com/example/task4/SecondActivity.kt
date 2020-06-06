4bc91fb6c286899bfb946b1669dca909d6175051
relfto <relftoforge@gmail.com>
2020-06-06 09:44:23 +0530

    Naviagtion

---
+++ app/src/main/java/com/example/task4/SecondActivity.kt
@@ -0,0 +1,44 @@
+package com.example.task4
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.GridView
+import android.widget.Toast
+import com.example.task4.R
+import com.example.task4.adapters.CarAdapters
+import com.example.task4.model.CarItem
+
+class SecondActivity : AppCompatActivity() , AdapterView.OnItemClickListener{
+
+    private var arrayList:ArrayList<CarItem> ? = null
+    private var gridView:GridView ? = null
+    private var carAdapters: CarAdapters? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        gridView = findViewById(R.id.grid_view_list)
+        arrayList = ArrayList()
+        arrayList = setValues()
+        carAdapters = CarAdapters(applicationContext, arrayList!!)
+        gridView?.adapter = carAdapters
+        gridView?.onItemClickListener = this
+    }
+
+    private fun setValues() : ArrayList<CarItem>{
+        var arrayList:ArrayList<CarItem> = ArrayList()
+
+        arrayList.add(CarItem(R.drawable.downloa, "Picture1"))
+        arrayList.add(CarItem(R.drawable.h1, "Picture2"))
+        arrayList.add(CarItem(R.drawable.h2, "Picture3"))
+        arrayList.add(CarItem(R.drawable.h3, "Picture4"))
+
+        return arrayList
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        var items:CarItem = arrayList!!.get(position)
+        Toast.makeText(applicationContext , items.name , Toast.LENGTH_LONG).show()
+    }
+}
